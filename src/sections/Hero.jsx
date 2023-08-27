730c5da81274eb3d9753c81f8594b9c5b3047280
nipun221 <2oics072@gbu.ac.in>
2023-08-27 17:06:51 +0530

    first commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-conatiner"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className='text-[25px] font-weight-500 font-montserrat text-blue-400'>
