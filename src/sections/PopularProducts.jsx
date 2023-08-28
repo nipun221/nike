efe692d0f0216eb0df60791d07a570667022a0d7
nipun221 <2oics072@gbu.ac.in>
2023-08-28 23:04:22 +0530

    first commit

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,6 +1,13 @@
+import { products } from '../constants';
+
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className='flex flex-col justify-start gap-5'>
+        <h2>Popular Products</h2>
+        <p></p>
+      </div>
+    </section>
   )
 }
 
