24abd1b0d7e2d34e1d1b5bd01f194c979bd5e756
nipun221 <2oics072@gbu.ac.in>
2023-08-30 20:37:13 +0530

    first commit

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -7,10 +7,10 @@ const Subscribe = () => {
       className='max-container flex justify-between items-center max-lg:flex-col gap-10'
     >
       <h3 className='text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold'>
-        Sign Up for
-        <span className='text-coral-red'> Updates </span>& Newsletter
+        Sign Up to our
+        <span className='text-purple-600'> Newsletter </span>
       </h3>
-      <div className='lg:max-w-[60%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full'>
+      <div className='lg:max-w-[70%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full'>
         <input type='text' placeholder='subscribe@nike.com' className='input' />
         <div className='flex max-sm:justify-end items-center max-sm:w-full'>
           <Button label='Sign Up' fullWidth />
